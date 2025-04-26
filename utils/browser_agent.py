45680aed3c07e9ba8d80932db009fc253b347f73
Samarth Chitley <maharanapratapsingh1576@gmail.com>
2025-04-26 18:01:41 +0530

    test commit

--- utils/browser_agent.py
+++ utils/browser_agent.py
@@ -1,1 +1,13 @@
-# Test.py
+from playwright.async_api import async_playwright
+
+class WebScrapper:
+    def __init__(self):
+        self.browser = None
+        self.playwright = None
+        self.page = None
+
+    async def init_browser(self):
+        self.browser = await async_playwright().start
+        self.playwright = await self.playwright.chromium.launch(headless=True)
+        self.page = await self.browser.new_page()
+        
